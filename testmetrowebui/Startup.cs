b3fad2da84f6e71c91f990e81135baf9d3500e20
Markus Rothhoeft <m.rothhoeft@msegmbh.de>
2016-06-10 15:02:58 +0200

    Initial Import

---
+++ testmetrowebui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testmetrowebui.Startup))]
+namespace testmetrowebui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
